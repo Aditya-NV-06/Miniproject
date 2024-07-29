cb5b9f723fcb84b8c8e80e009c1dbf2c0661d1e7
Aditya NV <adityanv4@gmail.com>
2024-07-29 14:32:31 +0530

    (added)js basics

---
+++ project18/Basic.js
@@ -0,0 +1,39 @@
+
+//comments
+
+
+//variables
+let rr=5;
+let cc=2;
+let ee=1;
+let ff=3;
+
+
+if(rr>cc){ 
+  ee++;
+}
+if(cc>=rr){ 
+  ff++;
+}
+
+let result=rr+cc+ee+ff;
+//to print the output in the console
+console.log(result);
+
+
+let ttt=23;
+
+//conditional
+
+if(ttt<0){ 
+  console.log('freezing');
+}
+else if(ttt>=0 && ttt<=10){ 
+   console.log('Cold');
+}
+else if(ttt>=11 && ttt<=25){ 
+  console.log('Warm');
+}
+else{ 
+  console.log("Hot");
+}
