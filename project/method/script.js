2f561a08b6f11be350114b3791e4411abc7b33ed
Aditya NV <adityanv4@gmail.com>
2024-08-02 14:58:57 +0530

    (added) js DOM

--- project/method/script.js
+++ project/method/script.js
@@ -1,33 +1,42 @@
 document.getElementById("loginForm").addEventListener('submit',function(event){
     event.preventDefault();
-   
-    const un=document.getElementById('username');
-    const pa=document.getElementById('password');
 
-    const unv=un.value.trim();
-    const pav=pa.value.trim();
+    const username=document.getElementById('username');
+    const password=document.getElementById('password');
 
-    if(unv==''){
-        setError(un,"password cannot me empty");
+    const usernamevalue=username.value.trim();
+    const passwordvalue=password.value.trim();
+    if (usernamevalue === '') {
+        setError(username,"User Name cannot be Empty !");
+        
     }
     else{
-        setsuccess(pa);
+        setSuccess(username);
     }
 
-        if(unv !=="" && pav !==""){
-            alert('login successful');
-        }
+    if (passwordvalue === '') {
+        setError(password,"Password cannot be Empty !");
+        
+    }
+    else{
+        setSuccess(password);
+    }
+
+    if(usernamevalue !== '' && passwordvalue !=='')
+        {
+        alert("Login Successful !");
+    }
 });
 
 function setError(element ,message){
     const formGroup =element.parentElement;
-    formGroup.className="form-group-error";
+    formGroup.className="form-group error";
     const small=formGroup.querySelector('small');
     small.innerText=message;
 }
 
-function setsuccess(element){
+function setSuccess(element){
     const formGroup =element.parentElement;
-    formGroup.className="form-group-error";
+    formGroup.className="form-group success";
 
 }
