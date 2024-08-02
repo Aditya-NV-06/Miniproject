b90aaf710ffe9584faf641f37ee3bd2317fa5888
Aditya NV <adityanv4@gmail.com>
2024-08-02 14:48:48 +0530

    (added) js DOM

---
+++ project/method/script.js
@@ -0,0 +1,33 @@
+document.getElementById("loginForm").addEventListener('submit',function(event){
+    event.preventDefault();
+   
+    const un=document.getElementById('username');
+    const pa=document.getElementById('password');
+
+    const unv=un.value.trim();
+    const pav=pa.value.trim();
+
+    if(unv==''){
+        setError(un,"password cannot me empty");
+    }
+    else{
+        setsuccess(pa);
+    }
+
+        if(unv !=="" && pav !==""){
+            alert('login successful');
+        }
+});
+
+function setError(element ,message){
+    const formGroup =element.parentElement;
+    formGroup.className="form-group-error";
+    const small=formGroup.querySelector('small');
+    small.innerText=message;
+}
+
+function setsuccess(element){
+    const formGroup =element.parentElement;
+    formGroup.className="form-group-error";
+
+}
